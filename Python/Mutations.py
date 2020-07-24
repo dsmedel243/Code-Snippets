3110e812505332d73a3dff25381a781569ecb773
dsmedel243 <32383628+dsmedel243@users.noreply.github.com>
2020-07-24 17:46:34 +0800

    Create Mutations.py

---
+++ Python/Mutations.py
@@ -0,0 +1,16 @@
+def mutate_string(string, position, character):
+    string = string[:position] + character + string[position+1:]
+    return string
+
+if __name__ == '__main__':
+    s = input()
+    i, c = input().split()
+    s_new = mutate_string(s, int(i), c)
+    print(s_new)
+    
+# Change the character at a given index and then print the modified string.
+#input
+#abracadabra
+#5 k
+#output
+#abrackdabra
