737446c3c09026f3251da80b1d50a70dfe37f38f
dsmedel243 <32383628+dsmedel243@users.noreply.github.com>
2020-07-24 17:35:35 +0800

    Create StringValidators.py

---
+++ Python/StringValidators.py
@@ -0,0 +1,18 @@
+if __name__ == '__main__':
+    s = input()
+    print(any(c.isalnum()  for c in s))
+    print(any(c.isalpha()  for c in s))
+    print(any(c.isdigit()  for c in s))
+    print(any(c.islower()  for c in s))
+    print(any(c.isupper()  for c in s))
+
+#check if string has alphanum, alpha, digit, lower and uppercase.
+
+#input
+#qA2
+#output
+#True
+#True
+#True
+#True
+#True
