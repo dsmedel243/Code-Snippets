344c99b610add9828d6eef73dfd15193ad0f5fe7
dsmedel243 <32383628+dsmedel243@users.noreply.github.com>
2020-07-24 17:24:30 +0800

    Create Matrix.py

---
+++ Python/Matrix.py
@@ -0,0 +1,13 @@
+matrix = [] 
+
+for i in range(5): 
+	
+	# Append an empty sublist inside the list 
+	matrix.append([]) 
+	
+	for j in range(5): 
+		matrix[i].append(j) 
+		
+print(matrix) 
+
+#Create a list of lists in python using the append function
