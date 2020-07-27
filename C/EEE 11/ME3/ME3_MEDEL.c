3ddc99d409cb01da789749e92c53a10196d551e1
dsmedel243 <32383628+dsmedel243@users.noreply.github.com>
2020-07-27 20:07:06 +0800

    Create ME3_MEDEL.c

---
+++ C/EEE 11/ME3/ME3_MEDEL.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+int main (){
+  char option, a;
+	int b1, b2, b3, b4, b5;
+	int period, loop1, loop2, loop3, loop4, line1, line2, line3, line4, line5, line6, line7, line8; 
+
+  while((option != 'A') && (option != 'B') && (option != 'C') && (option !='D')){
+    printf("Choose task by entering the corresponding letter.\nA: Get ASCII value of letters.\nB: Accumulate 5 integers.\nC: Generate waveform.\nD: Exit.\n");
+	  printf("What would you like to do? ");
+	  scanf(" %c", &option);
+	  switch(option){
+	
+	  case 'A':{
+		  while (!((a >= 'A') && (a <= 'Z')) || !((a >= 'a') && (a <= 'z'))){
+        printf("Input a letter: ");
+		    scanf(" %c", &a);    
+		    if (((a >= 'A') && (a <= 'Z')) || ((a >= 'a') && (a <= 'z'))){
+		      printf("You entered %c.\n", a);
+		      printf("The ASCII value of %c is '%c' = %d .\n", a, a, a);
+		      break;
+        }
+		    else{
+			    printf("This is not a letter of the alphabet.\n");
+		    }
+      }
+		  break;
+	  }	
+	  case 'B':{
+		  printf("Input 5 integers to accumulate: \n");
+		  scanf("%d", &b1);
+		  printf("Cumulative sum is %d.\n", b1);
+		  printf("Next integer: ");
+		  scanf("%d", &b2);
+		  printf("Cumulative sum is %d.\n", b1 + b2);
+		  printf("Next integer: ");
+		  scanf("%d", &b3);
+		  printf("Cumulative sum is %d.\n", b1 + b2 + b3);
+		  printf("Next integer: ");
+		  scanf("%d", &b4);
+		  printf("Cumulatove sum is %d.\n", b1 + b2 + b3 + b4);
+		  printf("Next integer: ");
+		  scanf("%d", &b5);
+		  printf("Cumulative sum is %d.\n", b1 + b2 + b3 + b4 + b5); 
+		  break;
+	    }
+	    case 'C':{
+		    printf("Enter period: ");
+		    scanf(" %d", &period);
+		    for(loop1=0; loop1 < (166 / period); loop1++){
+		      for(line1=0; line1 < (period/ 2) + 1; line1++){
+			    printf("x");	
+		      }
+		      for(line2=0; line2 < (period/ 2) - 1; line2++){
+			      printf(" ");	
+		      }
+		    }
+	      printf("\n");
+	      for(loop2=0; loop2 < (330 / period); loop2++){
+		      printf("x");
+		      for(line4=0; line4 < (period/ 2) - 1; line4++){
+			    printf(" ");	
+			    }		
+	      }
+	      printf("\n");
+	      for(loop3=0; loop3 < (330 / period); loop3++){
+		      printf("x");
+		      for(line6=0; line6 < (period/ 2) - 1; line6++){
+			    printf(" ");	
+			    }		
+	      }
+	      printf("\nx");
+	      for(loop4=0; loop4 < (166 / period); loop4++){
+		      for(line8=0; line8 < (period/ 2) - 1; line8++){
+			    printf(" ");	
+		      }
+		      for(line7=0; line7 < (period/ 2) + 1; line7++){
+			      printf("x");	
+		      }	
+	      }
+	      printf("\n");
+	      break;
+	    }	
+	    case 'D':{
+		    printf("Exit.\n");
+		    break;
+	    }	
+	    default:{	
+		    printf("Invalid Input.\n");
+	    }
+    }
+  }
+	return 0;
+}
+    
